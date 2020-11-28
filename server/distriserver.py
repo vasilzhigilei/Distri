742b1a5a03930be38d6329ee6d245e5017d86f52
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-11-28 00:32:03 -0500

    function routing

--- server/distriserver.py
+++ server/distriserver.py
@@ -12,3 +12,15 @@ def index():
     Main page for frontend
     """
     return render_template('index.html')
+
+@app.route('/api/generateroom')
+def generateroom():
+    # generate room here
+    return "room url code here"
+
+@app.route('r/<path:code>')
+def room(code):
+    # do room logic here
+    # return html render if from browser
+    # maybe return true bool for programatic connections to room
+    return None
