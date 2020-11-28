df70a6880a73f520d1cb41eec1266e835e31e90c
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-11-28 16:51:43 -0500

    socketio join/leave

--- server/distriserver.py
+++ server/distriserver.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO
 import secrets
+
+# initialize Flask app
 app = Flask(__name__)
 app.config.update(
     TEMPLATES_AUTO_RELOAD = True,
 )
 
+# initialize socketio
+socketio = SocketIO(app)
+
 @app.route('/')
 @app.route('/index')
 def index():
-    """
-    Main page for frontend
-    """
     return render_template('index.html')
 
 @app.route('/api/generateroom')
@@ -28,11 +31,25 @@ def room(code):
     # do room logic here
     # return html render if from browser
     # maybe return true bool for programatic connections to room
-    return code
+    return render_template('room.html', code=code, room=rooms[code].dict)
+
+@socketio.on('join')
+def on_join(data):
+    room = data['room']
+    join_room(room)
+
+@socketio.on('leave')
+def on_leave(data):
+    room = data['room']
+    leave_room(room)
 
 rooms = {}
 
 class Room:
     def __init__(self, code):
         self.code = code
+        self.connections = 0
         self.dict = {}
+
+if __name__ == '__main__':
+    socketio.run(app)
