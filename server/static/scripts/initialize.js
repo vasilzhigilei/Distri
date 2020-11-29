5404c0ad6b65cff0c6232667b3b53b986bcf5e12
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-11-29 17:38:44 -0500

    sorting table

--- server/static/scripts/initialize.js
+++ server/static/scripts/initialize.js
@@ -5,3 +5,7 @@ socket.emit('join', {'room': room});
 
 
 socket.emit('set', {'room': room, 'key':'Current i', 'value':321});
+socket.emit('set', {'room': room, 'key':'sum', 'value':'111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'});
+socket.emit('set', {'room': room, 'key':'Current i', 'value':54321});
+socket.emit('set', {'room': room, 'key':'Hello,', 'value':'World!'});
+socket.emit('set', {'room': room, 'key':'Current i', 'value':12345});
