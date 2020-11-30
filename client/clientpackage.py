7bb7fc97b2e0077b3843d5cfba9d8f76c81692f6
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-11-30 10:13:17 -0500

    name == main and added returns for funcs

--- client/clientpackage.py
+++ client/clientpackage.py
@@ -7,20 +7,27 @@ sio.connect('http://localhost:5000')
 @sio.on('confirm')
 def confirm(data):
     print(data)
+    return
 
 @sio.on('join response')
 def joinresponse(data):
     print("join response with room data: ", data)
     tableData = data
+    return
 
 @sio.on('updated data')
 def updateddata(data):
     for key in data:
         tableData[key] = data[key]
-
-room = str(input("Enter room code: "))
-sio.emit('join', {'room': room})
-
-sio.emit('set', {'room': room, 'key':'From Python', 'value':'with love!'});
+    return
 
 tableData = {}
+
+if __name__ == '__main__':
+    sio = socketio.Client()
+    sio.connect('http://localhost:5000')
+    
+    room = str(input("Enter room code: "))
+    sio.emit('join', {'room': room})
+
+    sio.emit('set', {'room': room, 'key':'From Python', 'value':'with love!'});
